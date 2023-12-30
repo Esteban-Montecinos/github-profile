c3112eb77a85ea6b7d1cc6ffc01477a071a50f24
Esteban-Montecinos <estebanmontecinos2001@gmail.com>
2023-12-30 00:17:29 -0300

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,26 @@
+import {nextui} from "@nextui-org/react";
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    },
+    colors: {
+      'navy': '#111729',
+      'azul': '#1D1B48',
+      'celeste': '#3662E3',
+      'celeste-oscuro': '#20293A',
+      'gris-oscuro': '#364153',
+      'gris-medio': '#4A5567',
+      'gris-claro': '#CDD5E0',
+      'gris': '#97A3B6'
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()]
 }
