0baa5eaecfa3567e57b3d06ce6bd16d899b9888a
Esteban-Montecinos <estebanmontecinos2001@gmail.com>
2023-12-30 11:16:40 -0300

    contrast ratio

--- src/app/components/chip.jsx
+++ src/app/components/chip.jsx
@@ -1,7 +1,7 @@
 export default function Chip({children, value}) {
   return (
-    <div className="flex gap-4 px-5 py-4 text-base divide-x divide-gris-medio bg-navy rounded-xl size-fit">
-      <span className="font-medium text-gris-medio">{value}</span>
+    <div className="flex gap-4 px-5 py-4 text-base divide-x divide-gris bg-navy rounded-xl size-fit">
+      <span className="font-medium text-gris">{value}</span>
       <span className="pl-4">{children}</span>
     </div>
   );
